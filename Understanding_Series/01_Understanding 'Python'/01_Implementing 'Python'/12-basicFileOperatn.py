de84a8969760f1b93a4ef0d2ca1c98d62011a824
Madhushree Purohit <46991711+Madhushreevp@users.noreply.github.com>
2023-02-08 17:31:32 +0530

    Add files via upload

---
+++ Understanding_Series/01_Understanding 'Python'/01_Implementing 'Python'/12-basicFileOperatn.py
@@ -0,0 +1,13 @@
+# Handling files
+import os
+inputFile= open('lyrics.txt','r')
+outputFile = open('copylyrics.txt','w')
+for line in inputFile:
+    outputFile.write(line)
+    print (line,end = '')
+   
+inputFile.close() 
+outputFile.close()
+os.remove('copylyrics.txt')
+# both opening and writing files should be in same folder
+# Song Reference : Movie - Om Shanti Om ( song rights owned by movie itself,not me )
